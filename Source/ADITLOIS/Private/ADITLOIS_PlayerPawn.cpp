315802586c0b1ef87fa3cae36aa08e9c5876e60b
Pruthweesha SA <pruthweeshasa@gmail.com>
2025-08-15 16:01:57 +0530

    added native class for player pawn

---
+++ Source/ADITLOIS/Private/ADITLOIS_PlayerPawn.cpp
@@ -0,0 +1,142 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#pragma once
+
+#include "ADITLOIS_PlayerPawn.h"
+#include "UObject/ConstructorHelpers.h"
+#include "GameFramework/PlayerState.h"
+#include "GameFramework/PawnMovementComponent.h"
+#include "GameFramework/FloatingPawnMovement.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Net/UnrealNetwork.h"
+
+// Sets default values
+AADITLOIS_PlayerPawn::AADITLOIS_PlayerPawn()
+{
+    // Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
+    PrimaryActorTick.TickInterval = 0.0f;
+    this->bReplicates = true;
+    this->SetReplicateMovement(true);
+
+    this->hitResult = FHitResult();
+
+    this->bUseControllerRotationYaw = false;
+
+    static ConstructorHelpers::FObjectFinder<USkeletalMesh> skeletalMeshFinder(TEXT("SkeletalMesh'/Game/Assets/SkeletalMeshes/SKM_Sherni.SKM_Sherni'"));
+
+    BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
+    BoxComponent->InitBoxExtent(FVector(70.0f, 24.0f, 54.0f));
+    BoxComponent->SetCollisionProfileName(UCollisionProfile::Pawn_ProfileName);
+
+    BoxComponent->CanCharacterStepUpOn = ECB_No;
+    BoxComponent->SetShouldUpdatePhysicsVolume(true);
+    BoxComponent->SetCanEverAffectNavigation(false);
+    BoxComponent->bDynamicObstacle = true;
+    this->RootComponent = BoxComponent;
+
+    FloatingPawnMovement = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("FloatingPawnMovement"));
+    FloatingPawnMovement->UpdatedComponent = BoxComponent;
+
+    this->GetMovementComponent()->SetIsReplicated(true);
+    this->GetRootComponent()->SetIsReplicated(true);
+
+    // if (skeletalMeshFinder.Succeeded())
+    // {
+    //     this->GetMesh()->SetSkeletalMeshAsset(skeletalMeshFinder.Object);
+    //     this->GetMesh()->SetRelativeLocation(FVector(0.0f, 0.0f, -85.0f));
+    //     this->GetMesh()->SetIsReplicated(true);
+    // }
+    springArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+    camera = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+
+    springArm->SetupAttachment(RootComponent);
+    camera->SetupAttachment(springArm);
+    springArm->SocketOffset = FVector(0.0f, 50.0f, 50.0f);
+    springArm->bUsePawnControlRotation = true;
+
+    Cast<UFloatingPawnMovement>(this->GetMovementComponent())->MaxSpeed = 300.0;
+}
+
+// Called when the game starts or when spawned
+void AADITLOIS_PlayerPawn::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void AADITLOIS_PlayerPawn::PossessedBy(AController *NewController)
+{
+    Super::PossessedBy(NewController);
+
+    if (HasAuthority())
+    {
+        SetReplicateMovement(true);
+        bReplicates = true;
+    }
+}
+
+// Called every frame
+void AADITLOIS_PlayerPawn::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    TObjectPtr<APlayerController> playerController = Cast<APlayerController>(GetController());
+
+    if (playerController)
+    {
+        playerController->GetPlayerViewPoint(startPoint, viewRotation);
+    }
+
+    startPoint = startPoint + viewRotation.Vector() * (springArm->TargetArmLength);
+    endPoint = startPoint + viewRotation.Vector() * (500.0f);
+
+    FCollisionQueryParams TraceParams(FName(TEXT("")), false, this);
+
+    bool bHit = GetWorld()->LineTraceSingleByChannel(this->hitResult, startPoint, endPoint, ECC_Visibility, TraceParams);
+
+    TObjectPtr<APlayerState> playerState = playerController ? playerController->PlayerState : nullptr;
+
+    if (HasAuthority())
+    {
+        interactionTarget = bHit ? this->hitResult.GetActor() : nullptr;
+    }
+    else
+    {
+        FHitResult localHitResult = this->hitResult;
+        ServerSetInteractionTarget(bHit, localHitResult);
+        // interactionTarget = bHit ? this->hitResult.GetActor() : nullptr;
+    }
+
+    if (GEngine && playerState)
+    {
+        int32 playerId = playerState->GetPlayerId();
+        FString hitDebugMessage = interactionTarget ? interactionTarget->GetName() : FString::Printf(TEXT("NullPtr"));
+        GEngine->AddOnScreenDebugMessage(playerId, 1.0f, FColor(0, 192, 64), FString::Printf(TEXT("Interaction Target: %s"), *hitDebugMessage));
+    }
+}
+
+void AADITLOIS_PlayerPawn::ServerSetInteractionTarget_Implementation(bool bHit, FHitResult localHitResult)
+{
+    this->interactionTarget = bHit ? localHitResult.GetActor() : nullptr;
+}
+
+// Called to bind functionality to input
+void AADITLOIS_PlayerPawn::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void AADITLOIS_PlayerPawn::GetLifetimeReplicatedProps(TArray<FLifetimeProperty> &OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+    DOREPLIFETIME(AADITLOIS_PlayerPawn, interactionTarget);
+    DOREPLIFETIME(AADITLOIS_PlayerPawn, springArm);
+    DOREPLIFETIME(AADITLOIS_PlayerPawn, camera);
+}
+
+TObjectPtr<USkeletalMeshComponent> AADITLOIS_PlayerPawn::GetMesh()
+{
+    return Mesh;
+}
---
+++ Source/ADITLOIS/Public/ADITLOIS_PlayerPawn.h
@@ -0,0 +1,87 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+
+#include "ADITLOIS_PlayerPawn.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class AActor;
+class APawn;
+class APlayerState;
+class UPawnMovementComponent;
+class UFloatingPawnMovement;
+class USkeletalMeshComponent;
+class UBoxComponent;
+struct FHitResult;
+
+UCLASS()
+class ADITLOIS_API AADITLOIS_PlayerPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AADITLOIS_PlayerPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void PossessedBy(AController *NewController) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent *PlayerInputComponent) override;
+
+	void GetLifetimeReplicatedProps(class TArray<FLifetimeProperty> &OutLifetimeProps) const override;
+
+	/** The main skeletal mesh associated with this Character (optional sub-object). */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USkeletalMeshComponent> Mesh;
+
+	/** Movement component used for movement logic in various movement modes (walking, falling, etc), containing relevant settings and functions to control movement. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UFloatingPawnMovement> FloatingPawnMovement;
+
+	/** The BoxComponent being used for movement collision (by FloatingPawnMovement). */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UBoxComponent> BoxComponent;
+
+	/** Returns Mesh subobject **/
+	TObjectPtr<USkeletalMeshComponent> GetMesh();
+
+	UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<USpringArmComponent> springArm = nullptr;
+
+	UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UCameraComponent> camera = nullptr;
+
+	UPROPERTY()
+	FHitResult hitResult;
+
+	UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<AActor> interactionTarget = nullptr;
+
+	UPROPERTY()
+	FVector startPoint;
+
+	UPROPERTY()
+	FRotator viewRotation;
+
+	UPROPERTY()
+	FVector endPoint;
+
+	UFUNCTION(Server, Reliable)
+	virtual void ServerSetInteractionTarget(bool bHit, FHitResult localHitResult);
+};
